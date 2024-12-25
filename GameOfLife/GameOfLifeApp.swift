db93bc2b2035ddd2427527ce8cd461c845998f21
TommiON <tommi.niittymies@gmail.com>
2024-12-25 22:17:34 +0200

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Tommi Niittymies on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
